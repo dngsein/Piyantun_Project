6507046a515b96f1feab0fa8da96d678264e25f6
dngsein <seindanang@gmail.com>
2022-04-18 12:05:20 +0700

    update routes untuk produk, profile, gambar

--- project-piyantun/routes/web.php
+++ project-piyantun/routes/web.php
@@ -11,38 +11,47 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Route::group(['middleware' => ['auth', 'cekrole:admin']], 
 
-	function () 
-	{
-		// Route::get('/dashboard', 'HomeController@dashboard')->name('dashboard');
-        Route::get('/home', 'HomeController@index')->name('home');
-		Route::resource('/katalog', 'HomeController@katalog');
+function () 
+{
+	Route::get('home', 'HomeController@index')->name('home');
+
+	Route::get('profile-pengguna', 'ViewPenggunaController@index')->name('akun_pengguna');
+	
+	Route::resource('produk', 'ProdukController');
+	Route::get('produk/{productID}/gambar', 'ProdukController@gambar')->name('gambar');
+	Route::get('produk/{productID}/tambah-gambar', 'ProdukController@tambahGambar'); 
+	Route::post('produk/gambar/{productID}', 'ProdukController@uploadGambar'); 
+	Route::delete('produk/gambar/{imageID}', 'ProdukController@hapusGambar')->name('hapusgb'); 
+
+
+	Route::get('profile-admin', 'UserProfileController@index')->name('adminProfile');
+	Route::get('profile-admin/edit', 'UserProfileController@edit')->name('adminProfile.edit');
+	Route::put('profile-admin/update', 'UserProfileController@update')->name('up.profile.admin');
+	
 });
 
-// Auth::routes();
+
 
 Route::group(['middleware' => ['auth', 'cekrole:user']], 
-	function () 
-	{
-		
-		Route::get('/home', 'HomeController@index')->name('home');
-		Route::get('/next', 'HomeController@next')->name('next');
-	});
-
-    
-    Auth::routes();
-	Route::get('/home', 'HomeController@index')->name('home');
+function () 
+{
 	
-	// Route::get('home', 'HomeController@index')->name('home');
+	Route::get('home', 'HomeController@index')->name('home');
+	Route::get('profile', 'UserProfileController@index');
+	Route::get('profile/edit', 'UserProfileController@edit');
+	Route::put('profile/update', 'UserProfileController@update')->name('up.profile');
+	
+
+});
 
 
+Auth::routes();
+Route::get('/home', 'HomeController@index')->name('home');
 
+Route::get('/', function () {
+	return view('welcome');
+});
 
-// Auth::routes();
-
-// Route::get('/home', 'HomeController@index')->name('home');
